429c8d4048fb70d14a3537563878eb0d0e51bf94
amerritt1999 <65575127+amerritt1999@users.noreply.github.com>
2020-06-13 13:59:50 -0500

    Add files via upload

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -4,13 +4,9 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import CategoricalNB
 import json
-import  numpy as np 
-from pathlib import Path
+import numpy as np 
+import os
 
-# List all files in directory using pathlib
-basepath = Path('AI Data/')
-files_in_basepath = (entry for entry in basepath.iterdir() if entry.is_file())
-arr2 = np.matrix('1,2;3,4')
 regions = ['Light World','Hyrule Castle','Eastern Palace' , 'Desert Palace' ,
            'Death Mountain' , 'Tower Of Hera' , 'Castle Tower' , 'Dark World','Dark Palace' , 
                'Swamp Palace' , 'Skull Woods' , 'Thieves Town','Ice Palace' , 'Misery Mire' , 
@@ -18,44 +14,48 @@ regions = ['Light World','Hyrule Castle','Eastern Palace' , 'Desert Palace' ,
 junkData = ['PieceOfHeart', 'HeartContainer', 'BossHeartContainer', 'OneRupee', 'FiveRupees', 'TwentyRupees', 
            'FiftyRupees', 'OneHundredRupees', 'ThreeBombs', 'TenBombs', 'TenArrows', 'BugCatchingNet',
            'ProgressiveShield']
-for item in files_in_basepath:
-    fp = open(item)
-    data = json.load(fp)
-    for i in regions:
-        li = list(data[i].items())
-        if li[0][1].split(':')[0] in junkData:
-            arr = np.matrix(['junk', li[0][0].split(':')[0].split(" - ")[0]])
-        else:
-            arr = np.matrix([li[0][1].split(':')[0], li[0][0].split(':')[0].split(" - ")[0]])
-        for j in range(1,len(li)):
-            if li[j][1].split(':')[0] in junkData:
-                arr = np.append(arr , [['junk', li[j][0].split(':')[0].split(" - ")[0]]] , axis = 0)
-            else:
-                arr = np.append(arr , [[li[j][1].split(':')[0], li[j][0].split(':')[0].split(" - ")[0]]] , axis = 0)
-        for item in arr:
-            arr2 = np.append(arr2, item, axis = 0)
-    
-arr3 = arr2[2:]
 
+counter2 = 0
 arr2 = np.matrix('1,2;3,4')
 fp = open("proj1.json")
 data = json.load(fp)
 for i in regions:
     li = list(data[i].items())
+    counter2+=len(data[i].items())
     if li[0][1].split(':')[0] in junkData:
-        arr = np.matrix(['junk', li[0][0].split(':')[0].split(" - ")[0]])
+        arr = np.matrix(['junk', li[0][0].split(':')[0]])
     else:
-        arr = np.matrix([li[0][1].split(':')[0], li[0][0].split(':')[0].split(" - ")[0]])
+        arr = np.matrix([li[0][1].split(':')[0], li[0][0].split(':')[0]])
     for j in range(1,len(li)):
         if li[j][1].split(':')[0] in junkData:
-            arr = np.append(arr , [['junk', li[j][0].split(':')[0].split(" - ")[0]]] , axis = 0)
+            arr = np.append(arr , [['junk', li[j][0].split(':')[0]]] , axis = 0)
         else:
-            arr = np.append(arr , [[li[j][1].split(':')[0], li[j][0].split(':')[0].split(" - ")[0]]] , axis = 0)
+            arr = np.append(arr , [[li[j][1].split(':')[0], li[j][0].split(':')[0]]] , axis = 0)
     for item in arr:
         arr2 = np.append(arr2, item, axis = 0)
 arr4 = arr2[2:]
 
-test = pd.DataFrame(arr3)
+counter = 0
+counter3 = 0
+matrix = np.empty(shape=(25000*counter2,2),dtype='object')
+for item in os.listdir('AI Data'):
+    fp = open('AI Data/'+item)
+    data = json.load(fp)
+    for i in regions:
+        li = list(data[i].items())
+        for j in range(0,len(li)):
+            if li[j][1].split(':')[0] in junkData:
+                matrix[counter3][0] = 'junk'
+                matrix[counter3][1] = li[j][0].split(':')[0]
+            else:
+                matrix[counter3][0] = li[j][1].split(':')[0]
+                matrix[counter3][1] = li[j][0].split(':')[0]
+            counter3+=1
+    if counter == 24999:
+        break
+    counter+=1 
+
+test = pd.DataFrame(matrix)
 test2 = pd.DataFrame(arr4)
 
 test.columns = ['Item', 'Location']
@@ -91,15 +91,14 @@ for i in dict2:
 inputs = test.Item.values.reshape(-1,1)
 target = test.Location.values
 
-X_train, X_test, y_train, y_test = train_test_split(inputs,target,test_size=0.2)
-
 clf = CategoricalNB()
 
-clf.fit(X_train, y_train)
+clf.fit(inputs, target)
 
 list0 = []
+
 for i in dict1:
-    if dict1[i] != 'junk':
+    if i != 'junk':
         list0.append(i)
 
 list2 = []
@@ -107,6 +106,11 @@ for i in list0:
     list2.append([dict1[i]])
 
 list1 = clf.predict_proba(list2)
+
+list3 = [list(test2.loc[test2['Location'] == 'Turtle Rock Medallion'].values[0])[1]]
+list3.append(list(test2.loc[test2['Location'] == 'Misery Mire Medallion'].values[0])[1])
+list3.append(list(test2.loc[test2['Location'] == 'Waterfall Bottle'].values[0])[1])
+list3.append(list(test2.loc[test2['Location'] == 'Pyramid Bottle'].values[0])[1])
 
 dict4 = {}
 j = 0
@@ -116,14 +120,15 @@ for j in range(len(list1)):
     counter = 0
     totalPercent = 0
     for i in top_idx[::-1]:
-        if dict3[str(i)] in test2.loc[test2['Item'] == 'unknown'].values:
-            if dict3[str(i)] not in dict4:
-                dict4[dict3[str(i)]] = str(top_values[len(list1[j])-counter-1]*100)
-            else:
-                if float(dict4[dict3[str(i)]]) < top_values[len(list1[j])-counter-1]*100:
+        if dict3[str(i)] not in list3:
+            if dict3[str(i)] in test2.loc[test2['Item'] == 'unknown'].values:
+                if dict3[str(i)] not in dict4:
                     dict4[dict3[str(i)]] = str(top_values[len(list1[j])-counter-1]*100)
-            totalPercent+=top_values[len(list1[j])-counter-1]*100
-            counter+=1
+                else:
+                    if float(dict4[dict3[str(i)]]) < top_values[len(list1[j])-counter-1]*100:
+                        dict4[dict3[str(i)]] = str(top_values[len(list1[j])-counter-1]*100)
+                totalPercent+=top_values[len(list1[j])-counter-1]*100
+                counter+=1
     j+=1
 
 counter = 0
@@ -132,3 +137,4 @@ for i in dict4:
     if counter == 10:
         break
     counter+=1
+print('Done')
